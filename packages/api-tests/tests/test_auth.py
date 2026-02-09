cef56ea4689e94bd2f6bc47f4e5623e6eedcce67
jjosephd <jjosephd804@gmail.com>
2026-02-09 16:45:09 -0500

    feat: add API test for user creation and update credentials in the user verification test.

--- packages/api-tests/tests/test_auth.py
+++ packages/api-tests/tests/test_auth.py
@@ -3,8 +3,8 @@ import pytest
 
 def test_verify_user_exists(base_url):
     user_login = {
-        'email': 'test@me.com',
-        'password': '123456',
+        'email': 'testuser4390@example.com',
+        'password': 'password123',
     }
     # Playwright 'form' corresponds to requests 'data' (form-encoded)
     response = requests.post(f"{base_url}/api/verifyLogin", data=user_login)
@@ -13,3 +13,34 @@ def test_verify_user_exists(base_url):
     assert response.status_code == 200
     assert data['responseCode'] == 200
     assert 'User exists' in data['message']
+
+def test_create_new_user(base_url):
+
+    req_params = {
+        'name': 'Test User',
+        'email': 'testuser43922@example.com',
+        'password': 'password123',
+        'title': 'Mr',
+        'birth_date': '1',
+        'birth_month': 'January',
+        'birth_year': '1990',
+        'firstname': 'Test',
+        'lastname': 'User',
+        'company': 'Test Company',
+        'address1': '123 Test Street',
+        'address2': 'Apt 4',
+        'country': 'United States',
+        'zipcode': '12345',
+        'state': 'California',
+        'city': 'Los Angeles',
+        'mobile_number': '1234567890'
+    }
+
+
+    response = requests.post(f"{base_url}/api/createAccount", data=req_params)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['responseCode'] == 201
+    assert 'User created' in data['message']
+
